0e95d6039829fe1cfee429d8bfda425f83809570
Paul Everitt <pauleveritt@me.com>
2016-03-31 10:12:26 -0400

    Parameterize the copyright.

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,9 @@ html_theme = 'sphinxstrap4'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    'github_user': 'pauleveritt'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [sphinxstrap4.get_path()]
